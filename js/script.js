876bb5e75f27543d5da7584b3bef2b5ef0c0d824
Evgeny <evgenii.salykin@gmail.com>
2021-10-29 17:10:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+// Операторы js
+
+// Логические операторы и, или && ||
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+// Инкремент и декримент
+// console.log('arr' + " - object");
+// console.log(4 + "5");
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+
+
+// Интерполяция
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// Запись данных в массив
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof (answers));
+
+// const result = confirm("Do you want an apple?"); ок или отмена т.е. (true or false)
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18"); можно ввести любой ответ
+// console.log(typeof (answer));
+// alert('hello');
+
+// let number = 4.6;
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// let und;
+
+// console.log(und);
+
+
+// // Объёкты
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+
+// // Массив - это частный случай объекта
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[3]);
+
+// console.log(-4/0); в результате будет - infinity
+// console.log('string' * 9); результатом будет NaN ( не число )
+
+// Переменные
+
+//  let number = 5; переменная let
+// const leftBorderWidth = 1; константа, но её можно изменить!!!
+
+// number = 10;
+// console.log(number);
+
+// const obj = { с помощью этого метода, можно изменить константу
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan'; var в строгом режиме использовать нельзя!
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+

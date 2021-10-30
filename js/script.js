bb4a04f11becf830e972eb7c19bf86bf247e5e17
Evgeny <evgenii.salykin@gmail.com>
2021-10-30 13:03:02 +0300

    first app

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 "use strict";
+// 1)
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+console.log(numberOfFilms);
+// 2)
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+};
+console.log(personalMovieDB);
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
 
 // Операторы js
 
 // Логические операторы и, или && ||
 
-const isChecked = false,
-    isClose = false;
+// const isChecked = false,
+//     isClose = false;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
 
 // Инкремент и декримент
 // console.log('arr' + " - object");

ddd68bf40dbb3b654116459079244e1c54f23d0d
Evgeny <evgenii.salykin@gmail.com>
2021-11-01 12:31:40 +0300

    Methods

--- js/script.js
+++ js/script.js
@@ -1,8 +1,179 @@
 "use strict";
 
+// practice Methods
+
+
+// end practice Methods
+
+// let numberOfFilms;
+
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+
+// start();
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
+
+//     },
+//     actors: {
+
+//     },
+//     genres: [],
+//     privat: false
+// };
+
+
+
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
+
+// // rememberMyFilms();
+
+
+
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log(' Просмотренно довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log('Вы классический зритель');
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log('Вы киноман');
+//     } else {
+//         console.log('Произошла ошибка');
+//     }
+// }
+
+// // detectPersonalLevel();
+
+// function showMyDB(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i - 1] = genre;
+//     }
+// }
+
+// writeYourGenres();
+//  Methods
+
+
+// const parce = "Привет мой юнный друг";
+
+// console.log(parce.slice(11, 20));
+
+// end Methods
+
+
+// function
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 20;
+// }
+
+
+// showFirstMessage('Hellow World!');
+
+// end fuction
+
+// Practise for and while
+
+// // 1)
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// // 2)
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
+
+//     },
+//     actors: {
+
+//     },
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+
+// if (personalMovieDB.count < 10) {
+//     console.log(' Просмотренно довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+
+// console.log(personalMovieDB);
+
+// let i = 0;
+
+// do {
+//     i++;
+// }
+
+// while (i < 2); {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+
+// end Practise
+
+
 // Циклы
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //     console.log(num);
@@ -16,14 +187,14 @@ let num = 50;
 
 // while (num < 55);
 
-for (let i = 1; i < 10; i++) {
-    if (i == 6) {
-        // break;
-        continue;
-    }
+// for (let i = 1; i < 10; i++) {
+//     if (i == 6) {
+//         // break;
+//         continue;
+//     }
 
-    console.log(i);
-}
+//     console.log(i);
+// }
 
 // end Циклы
 
